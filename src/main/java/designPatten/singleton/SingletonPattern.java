8aee8778cfece7d55fa9e8e71aac70a9fecbe002
zz_chong <1111>
2018-02-07 15:41:40 +0800

    单例模式

--- src/main/java/designPatten/singleton/SingletonPattern.java
+++ src/main/java/designPatten/singleton/SingletonPattern.java
@@ -3,14 +3,22 @@ package designPatten.singleton;
 /**
  * Created by pc on 2017/10/9.
  * 单例模式
- * 某个类在内存中只有一个实例
+ * 定义：确保某一个类只有一个实例，而且自行实例化并向整个系统提供这个实例
+ * 使用：饿汉式单例和懒汉式单例，建议使用饿汉式单例
+ * 应用：在Spring中，每个Bean默认就是单例，这样Spring容器可以管理这些Bean的生命周期。
+ * @author zz_chong
  */
 public class SingletonPattern {
 
-    //1.饿汉式单例
+    /**
+     * （1）饿汉式单例
+     */
     private static SingletonPattern singletonPattern = new SingletonPattern();
 
-    //保证不能通过正常渠道新建一个对象
+    /**
+     * 限制产生多个对象
+     * 通过使用private的构造函数确保了在一个应用中只产生一个实例，并且是自行实例化的。
+     */
     private SingletonPattern(){
 
     }
@@ -19,12 +27,24 @@ public class SingletonPattern {
         return singletonPattern;
     }
 
-    //2 懒汉式单例
+    /**
+     * 类中其他方法，尽量使用static
+     */
+    public static void doSomething(){
+
+    }
+    /**
+     * （2）懒汉式单例
+     */
     private static SingletonPattern singletonPattern1 = null;
 
-    // 在多线程高并发下会有线程安全问题  有可能会有多个实例生成 所以要加上synchronized
+
+    /**
+     * 在多线程高并发下会有线程安全问题  有可能会有多个实例生成 所以要加上synchronized
+     * @return SingletonPattern
+     */
     public synchronized static SingletonPattern getSingleton1(){
-        if(singletonPattern ==null){
+        if(singletonPattern1 ==null){
             return  new SingletonPattern();
         }
         return singletonPattern1;
